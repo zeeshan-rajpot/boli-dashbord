f11bc191573ec9e2226b32cdfb93b373e8d09bd2
AnasPirzada <anaspirzada510@gmail.com>
2024-04-18 01:17:58 +0500

    loylitycard changes form anas

--- src/App/Pages/Loyalty/AddLoylity.jsx
+++ src/App/Pages/Loyalty/AddLoylity.jsx
@@ -1,10 +1,56 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import { ToastContainer, toast } from 'react-toastify';
 import Navbar from '../../Components/Navbar.jsx';
 import SideBar from '../../Components/Sidebar.jsx';
+import { baseUrl } from '../../Components/constants.jsx';
+import 'react-toastify/dist/ReactToastify.css';
+import LoylityCard from './LoylityCard.jsx';
 export const AddLoylity = () => {
+  const [itemName, setItemName] = useState('');
+  const [points, setPoints] = useState('');
+
+  const handleAddItem = async () => {
+    try {
+      const token = localStorage.getItem('token');
+
+      const response = await axios.post(
+        `${baseUrl}/api/restaurant/addCard?action=add`,
+        {
+          amount: String(itemName),
+          points: parseInt(points),
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`, // Set auth token in the request headers
+          },
+        }
+      );
+      if (response && response.data && response.data.message) {
+        toast.success(response.data.message);
+      } else {
+        toast.success('Item added successfully');
+      }
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
   return (
     <div>
+      <ToastContainer
+        position='top-right'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='light'
+      />
       <Container fluid className='  h-100'>
         <Row>
           <Col
@@ -36,136 +82,93 @@ export const AddLoylity = () => {
                         className='textColor'
                         style={{ fontSize: '22px', fontWeight: '700' }}
                       >
-                        Add New Item
+                        Loyalty Card
                       </p>
-                    </div>
-                    <div className='my-3'>
-                      <p
-                        style={{
-                          color: '#4C535F',
-                          fontSize: '16px',
-                          fontWeight: '500',
-                        }}
-                      >
-                        Upload image of Item
-                      </p>
-
-                      <div
-                        className='d-flex my-3 flex-column justify-content-center align-items-center '
-                        style={{
-                          width: '132px',
-                          height: '130px',
-                          backgroundColor: '#FFFFFF',
-                          borderRadius: '8px',
-                          boxShadow:
-                            '1px 2px 11.100000381469727px 0px #0000001A',
-                        }}
-                      >
-                        <img src='/gallery-add.svg' alt='' />
-                        <p
-                          className='my-2'
-                          style={{
-                            color: '#4C535F',
-                            fontSize: '12px',
-                            fontWeight: '500',
-                          }}
-                        >
-                          Upload your photo
-                        </p>
-                      </div>
                     </div>
                   </div>
                 </div>
-                <Row className='px-0 md-px-2'>
-                  <Col lg={6}>
-                    <div className=' my-2 d-flex flex-column justify-content-start align-items-start'>
-                      <p
+                <LoylityCard />
+                {/* Create Offer */}
+                <div className='px-4 my-5'>
+                  <Row className='px-0 md-px-2'>
+                    <Col lg={6}>
+                      <div className=' my-2 d-flex flex-column justify-content-start align-items-start'>
+                        <p
+                          style={{
+                            fontWeight: '500',
+                            fontSize: '16px',
+                            color: '#4C535F',
+                          }}
+                        >
+                          Amount
+                        </p>
+                        <input
+                          className='w-100 border-0 p-3'
+                          style={{
+                            fontWeight: '500',
+                            fontSize: '12px',
+                            borderRadius: '8px',
+                            color: '#4C535F',
+                            boxShadow:
+                              '1px 2px 11.100000381469727px 0px #0000001A',
+                          }}
+                          type='text'
+                          placeholder='$60'
+                          value={itemName}
+                          onChange={e => setItemName(e.target.value)}
+                        />
+                      </div>
+                    </Col>
+                    <Col lg={6}>
+                      <div className=' my-2 d-flex flex-column justify-content-start align-items-start'>
+                        <p
+                          style={{
+                            fontWeight: '500',
+                            fontSize: '16px',
+                            color: '#4C535F',
+                          }}
+                        >
+                          points
+                        </p>
+                        <input
+                          className='w-100 border-0 p-3'
+                          style={{
+                            fontWeight: '500',
+                            fontSize: '12px',
+                            borderRadius: '8px',
+                            color: '#4C535F',
+                            boxShadow:
+                              '1px 2px 11.100000381469727px 0px #0000001A',
+                          }}
+                          type='text'
+                          placeholder='Enter points of the product'
+                          value={points}
+                          onChange={e => setPoints(e.target.value)}
+                        />
+                      </div>
+                    </Col>
+                  </Row>
+                  <Row className='my-3 '>
+                    <Col lg={3} xs={12} md={6}>
+                      <button
+                        className='border-0 w-100 mt-3  mt-md-0'
                         style={{
-                          fontWeight: '500',
-                          fontSize: '16px',
-                          color: '#4C535F',
-                        }}
-                      >
-                        Item name
-                      </p>
-                      <input
-                        className='w-100 border-0 p-3'
-                        style={{
-                          fontWeight: '500',
-                          fontSize: '12px',
                           borderRadius: '8px',
-                          color: '#4C535F',
-                          boxShadow:
-                            '1px 2px 11.100000381469727px 0px #0000001A',
-                        }}
-                        type='text'
-                        placeholder='Enter Product name'
-                      />
-                    </div>
-                  </Col>
-                  <Col lg={6}>
-                    <div className=' my-2 d-flex flex-column justify-content-start align-items-start'>
-                      <p
-                        style={{
-                          fontWeight: '500',
+                          color: '#00BF63',
                           fontSize: '16px',
-                          color: '#4C535F',
-                        }}
-                      >
-                        points
-                      </p>
-                      <input
-                        className='w-100 border-0 p-3'
-                        style={{
+                          padding: '11px 30px 11px 30px',
+                          boxShadow: ' 2px 4px 17.6px 0px #0000002B',
                           fontWeight: '500',
-                          fontSize: '12px',
-                          borderRadius: '8px',
-                          color: '#4C535F',
-                          boxShadow:
-                            '1px 2px 11.100000381469727px 0px #0000001A',
+                          backgroundColor: '#FFFFFF',
                         }}
-                        type='text'
-                        placeholder='Enter points of the product'
-                      />
-                    </div>
-                  </Col>
-                </Row>
-                <Row className='my-3 '>
-                  <Col lg={6} xs={12} md={6}></Col>
-                  <Col lg={3} xs={12} md={3}>
-                    {' '}
-                    <button
-                      className='w-100'
-                      style={{
-                        borderRadius: '8px',
-                        border: ' 1px solid #00BF63',
-                        color: '#00BF63',
-                        fontSize: '16px',
-                        padding: '11px 30px 11px 30px',
-                        fontWeight: '500',
-                        backgroundColor: '#FFFFFF',
-                      }}
-                    >
-                      Cancle
-                    </button>
-                  </Col>
-                  <Col lg={3} xs={12} md={3}>
-                    {' '}
-                    <button
-                      className='border-0 w-100 mt-3 mt-md-0'
-                      style={{
-                        borderRadius: '8px',
-                        color: '#FFFFFF',
-                        fontSize: '16px',
-                        padding: '11px 30px 11px 30px',
-                        fontWeight: '500',
-                        backgroundColor: '#00BF63',
-                      }}
-                    >
-                      Add Item
-                    </button>
-                  </Col>
-                </Row>
+                        onClick={handleAddItem}
+                      >
+                        Create New Offer
+                      </button>
+                    </Col>
+                    <Col lg={6} xs={12} md={3}></Col>
+                  </Row>
+                </div>
               </div>
             </Row>
           </Col>
@@ -174,4 +177,5 @@ export const AddLoylity = () => {
     </div>
   );
 };
+
 export default AddLoylity;
